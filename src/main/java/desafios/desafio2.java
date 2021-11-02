f20056e00c84749fe3b90904dd87ca19b1ddebda
annaandriani <anninhaandriani@gmail.com>
2021-11-02 20:50:34 -0300

    first commit

---
+++ src/main/java/desafios/desafio2.java
@@ -0,0 +1,14 @@
+package desafios;
+import java.util.Scanner;
+
+public class desafio2 {
+    public static void main(String[] args) {
+        int minutos;
+        Scanner input = new Scanner(System.in);
+        minutos = input.nextInt();
+
+
+        System.out.printf("%d minutos\n", 2 * minutos);
+        }
+    }
+
---
+++ src/main/java/desafios/desafio3.java
@@ -0,0 +1,18 @@
+package desafios;
+import java.util.Scanner;
+
+public class desafio3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num = entrada.nextInt();
+
+        for (int i = 0 ;( i < num -1); i ++) {
+
+            System.out.print("   Ho   ");
+
+            }
+        System.out.println("   Ho!   ");
+
+        }
+    }
+
